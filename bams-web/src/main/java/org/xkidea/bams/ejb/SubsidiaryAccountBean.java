5a0ca0f56dd6f321bf0adb269de97cb37249968e
xk <wang_xukun@163.com>
2021-05-22 07:46:22 +0800

    SubsidiaryBean

---
+++ bams-web/src/main/java/org/xkidea/bams/ejb/SubsidiaryAccountBean.java
@@ -0,0 +1,23 @@
+package org.xkidea.bams.ejb;
+
+import org.xkidea.bams.entity.SubsidiaryAccount;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class SubsidiaryAccountBean extends AbstractFacade<SubsidiaryAccount> {
+
+    @PersistenceContext(unitName = "bamsPU")
+    private EntityManager em;
+
+    public SubsidiaryAccountBean() {
+        super(SubsidiaryAccount.class);
+    }
+
+    @Override
+    public EntityManager getEntityManager() {
+        return em;
+    }
+}
