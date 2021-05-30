6a0692e4895e115d2074d5ae8e6fbf7841536c94
xk <wang_xukun@163.com>
2021-05-31 01:22:37 +0800

    DetailRecordBean...

---
+++ bams-web/src/main/java/org/xkidea/bams/web/DetailRecordController.java
@@ -0,0 +1,14 @@
+package org.xkidea.bams.web;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named(value = "detailRecordController")
+@SessionScoped
+public class DetailRecordController implements Serializable {
+    private static final long serialVersionUID = -7176059911038677633L;
+    private static final String BUNDLE = "bundles.Bundle";
+
+
+}
